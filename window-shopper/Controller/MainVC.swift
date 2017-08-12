59c149b9a225945b67f97b33eb0d03ad4d5a03c5
panmajki <michal@Pro-panmajki.local>
2017-08-12 18:08:43 +0200

    lesson-03 finished

--- window-shopper/Controller/MainVC.swift
+++ window-shopper/Controller/MainVC.swift
@@ -13,6 +13,7 @@ class MainVC: UIViewController {
     @IBOutlet weak var wageTxt: CurrencyTxtField!
     @IBOutlet weak var priceTxt: CurrencyTxtField!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
